07bb1fac3eda722f58cd9c8d08b56b423cf982cf
Kyle <56949068+mrzakch@users.noreply.github.com>
2019-11-03 19:58:57 -0500

    Added profile search to search.
    You can now @ a user to get their stats.

--- commands/profile.js
+++ commands/profile.js
@@ -12,14 +12,20 @@ const Discord = require('Discord.js');
 //Command
 exports.command=function(message, args, temptotals, exp) {
     var loadbar = "";
-    var nxt = main.exp_curve(exp[message.author.id]["lvl"] + 1);
+    var target=message.member.displayName;
+    var targetId=message.author.id;
+    if (message.mentions.members.first()){
+        targetId=message.mentions.members.first().id;
+        target=message.mentions.members.first();
+    }
+    var nxt = main.exp_curve(exp[targetId]["lvl"] + 1);
     try {
         var sum = 0;
-        for (var i = 0; i <= exp[message.author.id]["lvl"]; i++) {
+        for (var i = 0; i <= exp[targetId]["lvl"]; i++) {
             sum += main.exp_curve(i);
         }
         for (var i = 1; i <= 10; i++) {
-            if ((exp[message.author.id]["raw"] - sum) / nxt > (i / 10))
+            if ((exp[targetId]["raw"] - sum) / nxt > (i / 10))
                 loadbar += "▰";
             else loadbar += "▱";
         }
@@ -28,19 +34,19 @@ exports.command=function(message, args, temptotals, exp) {
         console.log(err);
     }
     var name = "";
-    if (message.channel.type === 'text') name = message.member.displayName;
+    if (message.channel.type === 'text') name = target.displayName;
     else name = message.author.username;
     var sorted = main.sortExp(exp)
     var rank = 1
     for (var i = 0; i < sorted.length; i++) {
-        if (sorted[i][0] == message.author.id)
+        if (sorted[i][0] == targetId)
             break;
         else rank++;
     };
     var embed = new Discord.RichEmbed()
         .setColor(main.set.defaultcolor)
         .setTitle('**' + (name) + '\'s Statistics**')
-        .setDescription("**Cash:** " + temptotals[message.author.id] + "\n**Level:** " + exp[message.author.id]["lvl"] + "\n" + loadbar + "\n(Rank: " + rank + "/" + sorted.length + ")")
+        .setDescription("**Cash:** " + temptotals[targetId] + "\n**Level:** " + exp[targetId]["lvl"] + "\n" + loadbar + "\n(Rank: " + rank + "/" + sorted.length + ")")
         .setTimestamp();
     return message.channel.send("<@" + message.author.id + ">", embed);
 }
