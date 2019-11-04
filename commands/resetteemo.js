01c47b4de1267a278e2ea22535f10554d058f07c
Kyle <56949068+mrzakch@users.noreply.github.com>
2019-11-04 16:26:24 -0500

    Secured Resetteemo
    Same reasoning, don't want nefarious parties.

--- commands/resetteemo.js
+++ commands/resetteemo.js
@@ -7,10 +7,19 @@ exports.helpText = "Resets cash values.";
 
 //Requirements
 var main = require('../main.js');
+var config = require('../config.json');
 const Discord = require('Discord.js');
+
+function isSuperAdmin(id){
+    if (config.adminids.includes(id))
+            return true;
+        return false;
+}
 
 //Command
 exports.command = function (message, args, temptotals, exp) {
+    if(!isSuperAdmin(message.author.id))
+        return message.channel.send("You don't have permission to do that!");
     main.store.updateItem('totals', {});
     return;
 }
