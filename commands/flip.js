9a10758b4856a0e94ee6e7477aa47df880f4ab2e
Kyle <56949068+mrzakch@users.noreply.github.com>
2019-11-03 19:28:26 -0500

    Coin Flip Image
    Added image instead of text for flip command.

--- commands/flip.js
+++ commands/flip.js
@@ -11,11 +11,14 @@ const Discord = require('Discord.js');
 
 //Command
 exports.command = function (message, args, temptotals, exp) {
-    var txt = (Math.random() <= .5) ? "Heads" : "Tails";
+    var state = (Math.random() <= .5)
     var embed = new Discord.RichEmbed()
         .setTitle("Coin Flip")
-        .setDescription(txt)
         .setColor(main.set.defaultcolor);
+    if (state)
+        embed.setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/US_Nickel_2013_Obv.png/110px-US_Nickel_2013_Obv.png');
+    else
+        embed.setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/US_Nickel_2013_Rev.png/110px-US_Nickel_2013_Rev.png');
     return message.reply(embed);
 }
 
